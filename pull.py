3fe945da971097581a8b96dcd91b6a3ffb613286
ebberg <infinitime@c-ebberg.de>
2023-11-19 23:37:52 +0100

    Syntax error

--- pull.py
+++ pull.py
@@ -36,7 +36,7 @@ while True:
     for key, data in versions.items():
         # Pull
         name = data["image"][data['image'].rfind("/")+1:data['image'].rfind(":")]
-        tag = data["image"][data.rfind(":")+1:]
+        tag = data["image"][data['image'].rfind(":")+1:]
         result = subprocess.run(["skopeo", skopeo_options, "copy", "docker://"+data["image"]+":"+data["tag"], "docker://"+os.environ['OCP_REGISTRY_URL']+"/"+os.environ['OCP_PROJECT']+"/"+name+":"+tag])
         # Tag
         #result = subprocess.run(["podman", "tag", data["image"]+":"+data["tag"], os.environ['OCP_REGISTRY_URL']+"/"+os.environ['OCP_PROJECT']+"/"+os.environ['OCP_IMAGESTREAM']+":"+os.environ['OCP_IMAGE_TAG']])
