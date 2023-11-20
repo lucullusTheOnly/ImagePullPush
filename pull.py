ad988c7229804030e13829afbd30eaaf49dc34b0
ebberg <infinitime@c-ebberg.de>
2023-11-20 12:51:40 +0100

    Logging

--- pull.py
+++ pull.py
@@ -40,7 +40,7 @@ while True:
     for key, data in versions.items():
         # Pull
         name = data["image"][data['image'].rfind("/")+1:]
-        print(data["image"] + " --> " +str(name)+":"+str(data["tag"]), file=sys.stdout)
+        print("docker://"+data["image"]+":"+data["tag"]+" -> "+ "docker://"+os.environ['OCP_REGISTRY_URL']+"/"+os.environ['OCP_PROJECT']+"/"+name+":"+data["tag"]), file=sys.stdout)
         result = subprocess.run(["skopeo", "copy", skopeo_options, "docker://"+data["image"]+":"+data["tag"], "docker://"+os.environ['OCP_REGISTRY_URL']+"/"+os.environ['OCP_PROJECT']+"/"+name+":"+data["tag"]])
         # Tag
         #result = subprocess.run(["podman", "tag", data["image"]+":"+data["tag"], os.environ['OCP_REGISTRY_URL']+"/"+os.environ['OCP_PROJECT']+"/"+os.environ['OCP_IMAGESTREAM']+":"+os.environ['OCP_IMAGE_TAG']])
