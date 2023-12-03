f8b82a97463f376fef6e32fddc7d13a5ab972609
lucullusTheOnly <33200864+lucullusTheOnly@users.noreply.github.com>
2023-12-03 12:11:33 +0100

    type of octal file permissions

--- pull.py
+++ pull.py
@@ -31,7 +31,7 @@ if 'GIT_REPO' in os.environ.keys():
     if 'GIT_SSHKEY' in os.environ.keys():
         with open('/.ssh/id_rsa','w') as f:
             f.write(os.environ['GIT_SSHKEY'])
-        os.chmod('/.ssh/id_rsa', '0600')
+        os.chmod('/.ssh/id_rsa', 0o600)
     time.sleep(interval)
     repo = os.environ['GIT_REPO']
     match = re.match('^[a-z]+://', repo)
